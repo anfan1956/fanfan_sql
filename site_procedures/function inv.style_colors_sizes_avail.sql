72fbebcd61f6d86199f811fc1aa2842b02938c88
anfan1956 <92575765+anfan1956@users.noreply.github.com>
2022-05-19 10:24:55 +0300

    функция поправлена, добавлена текущая цена

--- site_procedures/function inv.style_colors_sizes_avail.sql
+++ site_procedures/function inv.style_colors_sizes_avail.sql
@@ -1,6 +1,10 @@
+use fanfan
+go
 /*
-На странице товара ( я так понимаю, когда страница обновляется)
-выполняется запрос :
+На странице товара ( я так понимаю, когда страница обновляется, а также, когда обновляется страница корзины)
+выполняется запрос : (пример со стилем 19321)
 */
 declare @styleid int = 19321 ;
-select color, size, sizeid, qty from inv.style_colors_sizes_avail (@styleid) ORDER BY color ASC, sizeid
+select color, size, sizeid, qty, price 
+from inv.style_colors_sizes_avail (@styleid) ORDER BY color ASC, sizeid
+use fanfan
