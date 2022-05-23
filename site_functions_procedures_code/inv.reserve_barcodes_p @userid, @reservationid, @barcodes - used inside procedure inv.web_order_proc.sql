1bdd8810e39bdba33bc614ce80089fd19ba324ab
anfan1956 <92575765+anfan1956@users.noreply.github.com>
2022-05-23 23:30:50 +0300

    creating procedure inv.reserve_barcodes_p - init modified inv.site_order_barcodes_f modified inv.web_order_proc

---
+++ site_functions_procedures_code/inv.reserve_barcodes_p @userid, @reservationid, @barcodes - used inside procedure inv.web_order_proc.sql
@@ -0,0 +1,59 @@
+USE [fanfan]
+GO
+
+
+ALTER proc [inv].[reserve_barcodes_p] 
+		@userid int, 
+		@reservationid int,
+		@barcodes inv.barcode_type readonly, 
+		@message varchar(max) output as 
+
+set nocount on;
+	begin try
+		begin transaction;
+			with s (reservationid, barcodeid, price, barcode_discount, customer_discount, order_stateid) as (
+			select  	
+				@reservationid, b.barcodeid, p.price, isnull(p.discount, 0), club.cust_discount_f(@userid),
+				inv.site_order_state_id ('reserved')
+			from @barcodes b
+				cross apply  inv.prices_barcodeid_f(b.barcodeid) p
+			)
+			insert inv.site_reservation_set (reservationid, barcodeid, price, barcode_discount, customer_discount, order_stateid)
+			select reservationid, barcodeid, price, barcode_discount, customer_discount, order_stateid from s;
+			--select * from inv.site_reservation_set s where s.reservationid = @reservationid;
+
+			commit transaction;
+		return 1
+	end try
+	begin catch
+		select @message =  ERROR_MESSAGE()
+		rollback transaction
+		return -1
+	end catch
+	go
+
+	declare @date datetime = current_timestamp, @userid int =17205, @transactionid int = 72225;
+	declare @goods inv.web_order_type, @message varchar (max), @r int; 
+	insert @goods (styleid, size, color, qty) values 
+		(19212, 'XS', 'cappuchino', 1), 
+		(19212, 'L', 'PENCIL', 2),
+		(19314, 'L', '677 mist wi', 1), 
+		(19321, 'M', 'CHARCOAL FUME', 1);
+	declare @barcodes inv.barcode_type;
+	insert @barcodes select barcodeid from inv.site_order_barcodes_f(@goods);
+
+--exec @r  = inv.reserve_barcodes_p 
+--	@userid  = @userid,
+--	@reservationid = @transactionid,
+--	@barcodes = @barcodes, 
+--	@message = @message  output;
+--select @r, @message;
+select * from inv.site_reservations
+select * from inv.site_reservation_set
+--truncate table inv.site_reservation_set
+
+
+
+
+
+
--- site_functions_procedures_code/inv.site_order_barcodes_f (@goods inv.web_order_type readonly).sql
+++ site_functions_procedures_code/inv.site_order_barcodes_f (@goods inv.web_order_type readonly).sql
@@ -5,7 +5,7 @@ ALTER function [inv].[site_order_barcodes_f] (@goods inv.web_order_type readonly
 	_ab (barcodeid) as (
 		select r.barcodeID 
 		from inv.v_remains r 
-			join org.active_divisions_f(GETDATE()) a on a.divisionID=r.divisionID
+			join org.active_retail_divisions_f(GETDATE()) a on a.divisionID=r.divisionID
 		where r.logstateID = inv.logstate_id('IN-WAREHOUSE')
 	)
 	, _s(styleid, size, color, qty, barcodeid, num) as (
@@ -28,5 +28,25 @@ insert @goods (styleid, size, color, qty) values
 	(19314, 'L', '677 mist wi', 1), 
 	(19321, 'M', 'CHARCOAL FUME', 1);
 
-select barcodeid, styleid from inv.site_order_barcodes_f(@goods)
+select barcodeid, styleid from inv.site_order_barcodes_f(@goods);
+
+	with 
+	_ab (barcodeid) as (
+		select r.barcodeID 
+		from inv.v_remains r 
+			join org.active_retail_divisions_f(getdate()) a on a.divisionID=r.divisionID
+		where r.logstateID = inv.logstate_id('IN-WAREHOUSE')
+	)
+	, _s(styleid, size, color, qty, barcodeid, num) as (
+	select g.styleid, g.size, g.color, g.qty,  b.barcodeID, ROW_NUMBER() over(partition by b.sort_barcodeid order by b.barcodeid) num
+	from @goods g
+		join inv.barcodes b on b.styleID=g.styleid
+		join inv.sizes sz on sz.sizeID=b.sizeID and g.size=sz.size
+		join inv.colors cl on cl.colorID=b.colorID and cl.color=g.color	
+		join _ab a on a.barcodeid=b.barcodeID
+	)
+	select barcodeid, styleid, s.color, s.size, num, s.qty
+	from _s s 
+	where num <=qty
+
 
