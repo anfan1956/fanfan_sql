f28f1b7c4775697fd1410d3ad6afd29bb6d7b508
anfan1956 <92575765+anfan1956@users.noreply.github.com>
2022-05-24 18:16:07 +0300

    init

---
+++ sales/SQLQuery85.sql
@@ -0,0 +1,8 @@
+use fanfan
+go
+
+select t.*, tt.transactiontype 
+from inv.transactions t 
+	join inv.transactiontypes tt on tt.transactiontypeID=t.transactiontypeID
+order by 1 desc
+select * from inv.inventory i where i.transactionID= 72253
