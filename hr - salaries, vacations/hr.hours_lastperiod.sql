78f3e7ba45bcdee33ecd80a8a7eafbe0d5ecf759
anfan1956 <92575765+anfan1956@users.noreply.github.com>
2022-07-31 23:13:59 +0300

    Исправление: функция hr.hours_lastperiod считала
    отработанные часы за все время, а нужно было только за
    один период

--- hr - salaries, vacations/hr.hours_lastperiod.sql
+++ hr - salaries, vacations/hr.hours_lastperiod.sql
@@ -7,9 +7,13 @@ SET QUOTED_IDENTIFIER ON
 GO
 ALTER function [hr].[hours_lastperiod](@startdate date) returns table as return
 
-with  _periods as (
-	select start_date, salary_date 
-	from inv.periods_closed_f(@startdate)
+with _enddate (enddate) AS (
+	SELECT sd.salary_date 
+	FROM hr.salary_dates sd
+	WHERE sd.salary_date >=DATEADD(d, -1, @startdate)
+	ORDER BY sd.salary_date ASC
+	OFFSET 1 ROWS
+	FETCH NEXT 1 ROWS ONLY
 )
 , _ids as (
 		select distinct s.personid
@@ -24,6 +28,7 @@ with  _periods as (
 				case 
 					when checktype=1 
 						and CAST(checktime as time(0))<cast('10:00' as time(0)) 
+
 						and superviserID is null 
 							then DATEADD(hh, 10, dbo.justdate(checktime))
 					when checktype=0 
@@ -32,10 +37,13 @@ with  _periods as (
 							 then DATEADD(hh, 22, dbo.justdate(checktime))
 					else checktime end checktime, 
 					org.workstation_client_id(a.workstationID, cast(a.checktime as date)) client_id, 
-					p.salary_date
+					e.enddate
 		from org.attendance a 
 			join _ids i on i.personid=a.personID
-			join _periods p on cast(a.checktime as date) between  p.start_date and p.salary_date
+			JOIN _enddate e ON e.enddate>=CAST(a.checktime AS date)
+			--join _periods p on cast(a.checktime as date) between  p.start_date and p.salary_date
+			WHERE CAST(a.checktime AS DATE) >=@startdate
+
 	)
 	select 
 		distinct a.salary_date, clientid, a.personID,
@@ -43,3 +51,9 @@ with  _periods as (
 		/* с детализацией по  дням */
 	--	distinct a.personID, sum(convert(money, a.checktime)*(1 - 2 * a.checktype)*24) over(partition by a.personid) hr
 	from _verified a
+GO
+----функция считала отработанные часы за все время, а не за период
+
+DECLARE @startdate DATE = '20220701';
+SELECT * FROM hr.hours_lastperiod(@startdate) hl
+WHERE hl.personID= 67
