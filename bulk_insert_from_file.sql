27ac6e8f87d7747e0f0b2f7d32f5efc48c5815a2
anfan1956 <92575765+anfan1956@users.noreply.github.com>
2022-08-07 15:10:04 +0300

    How to bulk insert into sql table
    from .txt file

---
+++ bulk_insert_from_file.sql
@@ -0,0 +1,21 @@
+﻿if OBJECT_ID('tmp.sms')is not null drop table tmp.sms 
+create table tmp.sms(
+	phone CHAR(11) , 
+	country VARCHAR (25), 
+	operator VARCHAR(25), 
+	region VARCHAR(155), 
+	mcc VARCHAR (25), 
+	mnc VARCHAR (25), 
+	tz VARCHAR (25)
+)
+go
+BULK INSERT tmp.sms
+FROM "D:\Development\jobs\operators.txt"
+WITH
+(
+	CODEPAGE = '65001',
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+)
+SELECT * FROM tmp.sms s
