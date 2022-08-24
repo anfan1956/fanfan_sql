20b7b4e660f8ecd5cf7491a82b6051e5625ae282
anfan1956 <af.fanfan.2012@gmail.com>
2022-08-24 15:13:16 +0300

    added commision to show if commission is charged

--- hr - salaries, vacations/hr.salary_charge_f.sql
+++ hr - salaries, vacations/hr.salary_charge_f.sql
@@ -10,14 +10,15 @@ with s (commissions) as (
 	select sum(sales * rate) 
 	from hr.sales_commissions_f(@startdate)
 ) 
-, f (personID, hrs, clientid, commissions,hourly_wage, min_wage, PIT,SocTax, charge_date) as (
+, f (personID, hrs, clientid, commissions,hourly_wage, min_wage, PIT,SocTax, charge_date, commission) as (
 	select h.personID, h.hrs, h.clientid, 
 		h.hrs/sum(h.hrs) over () * s.commissions,
 		hr.actual_hourcharge_salespeople(@startdate) * h.hrs,
 		h.hrs * hr.parameter_value_f('минималка/час', null) * w.has_MW ,
 		h.hrs * hr.parameter_value_f('минималка/час', null) * hr.parameter_value_f('ставка НДФЛ', null) * w.has_MW,
 		h.hrs * hr.parameter_value_f('минималка/час', null) * hr.parameter_value_f('ЕСН', null) * w.has_MW, 
-		@update
+		@update, 
+		h.commission
 		--h.hrs/sum(h.hrs) over () * s.commissions + hr.actual_hourcharge_salespeople(@startdate) *h.hrs total
 ---------------вот здесь была ошибка. Функция считает все деньги начисленные после startdate, а не только за период
 ---------- Ошибку поправил в предыдущем коммите
@@ -31,7 +32,7 @@ select charge_date, personID, clientid,
 	anfan_release.acc.journalid_func('hard cash'), 
 	anfan_release.acc.transaction_details_id ('комиссионные'), 
 	commissions,
-	anfan_release.acc.accountid_func('комиссионные', 'RUR') from f union
+	anfan_release.acc.accountid_func('комиссионные', 'RUR') from f WHERE f.commission='True' UNION
 select charge_date, personID, clientid, 
 	anfan_release.acc.journalid_func('hard cash'),
 	anfan_release.acc.transaction_details_id ('оклад, нал'), 
