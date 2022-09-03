6518e114aa2acaa8bc8ef0fbea60df9ee9637214
anfan1956 <af.fanfan.2012@gmail.com>
2022-09-03 15:19:33 +0300

    some chages, rent_objectid included
    D  O     N  O  T     R  U  N  !!!

--- finance/rent_tables.sql
+++ finance/rent_tables.sql
@@ -1,7 +1,11 @@
 ﻿use fanfan
 go
+
+-- unless there is change in table structure  - no more runs!
+
+
 declare @start_date date = '20220101'
-select * from fin.gross_p_l_func(@start_date)
+--select * from fin.gross_p_l_func(@start_date)
 --select * from cmn.numbers
 if OBJECT_ID('fin.rent') is not null drop table fin.rent
 if OBJECT_ID('fin.rent_objects') is not null drop table fin.rent_objects
@@ -26,28 +30,23 @@ select * from fin.rent_objects;
 
 create table fin.rent (
 	divisionid int not null,
+	rent_objectid int not null,
 	date_start date not null,
 	date_finish date null,
 	currencyid int not null, 
 	currency_rate dec (8, 5) null, 
-	rent_per_meter_year dec(10, 8) not null,
+	rent_per_meter_year money null,
+	fixed_per_month money default (0),
 	VAT dec (3,2) default (0),
 	turnover_rate dec(3,2) default (0),
 	constraint pk_rent primary key (divisionid, date_start)
 )
 go 
-
-
-select * from org.contractors c
-where 
-	c.contractor like '%дрим%' or
-	c.contractor like 'dream' or
-	c.contractor like '%кро%' or
-	c.contractor like '%балтия%' 
-
-
-
-
-
-
-
+insert fin.rent (divisionid, rent_objectid, date_start, currencyid, rent_per_meter_year, VAT, turnover_rate )
+select 
+	18, 1, '20220819', 840, 50, .20, .15
+insert fin.rent (divisionid, rent_objectid, date_start, currencyid, fixed_per_month,  VAT, turnover_rate )
+values  
+	(25, 2, '20220801', 840, 200, .20, .13), 
+	(27, 3, '20220801', 840, 200, .20, .13)
+select * from fin.rent
