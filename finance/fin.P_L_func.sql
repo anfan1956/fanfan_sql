a0c0ae73a61bfeaa0f357663c1d14fdf7178fec1
anfan1956 <af.fanfan.2012@gmail.com>
2022-09-05 00:01:19 +0300

    fin.P_L_func added other expenses and cross apply periods

--- finance/fin.P_L_func.sql
+++ finance/fin.P_L_func.sql
@@ -2,6 +2,8 @@
 go
 if OBJECT_ID('fin.P_L_func') is not null drop function fin.P_L_func
 go
+declare @start_date date = '20220731';
+go
 create function fin.P_L_func(@start_date date) returns table as return
 
 
@@ -41,9 +43,9 @@ create function fin.P_L_func(@start_date date) returns table as return
 		s.amount * r.turnover_rate, ro.footage * 
 			isnull(r.rent_per_meter_year, 0 )/12 * cr.rate )*(1+VAT) as money)
 			RENT,
-		iif(s.amount * r.turnover_rate> 
+		cast(iif(s.amount * r.turnover_rate> 
 		ro.footage * isnull(r.rent_per_meter_year, 0 )/12 * cr.rate, 
-		'turnover', 'base') rent_type,
+		'turnover', 'base') as varchar(15)) rent_type,
 		s.month_num, 
 		--cast(- p.n_days * 4000 as money) PRLL,
 		--cast(-s.amount * 0.025 as money) CoMiSN
@@ -55,10 +57,30 @@ create function fin.P_L_func(@start_date date) returns table as return
 		left join fin.rent_objects ro on ro.rent_objectid= r.rent_objectid
 		join cmn.currentrates cr on cr.currencyID = r.currencyid
 		join hr.acrued_wages_f() ac on ac.divisionid=s.divisionid and ac.fin_period= s.fin_period
-)
+), unpvt as (
 select divisionid, fin_period, sales_month, sales_year, rent_objectid, rent_type, month_num, amount, account 
 from (select * from f) f 
 unpivot ( amount for account in (SALES, COGS, RENT, PRLL, CoMiSN)) as unpvt
+)
+, _other as (
+select sum(e.amount) amount, org.division_id('ОФИС') divisionid, oe.expense_type account, p.fin_period
+from fin.other_expenses e
+	join fin.other_exense_types oe on oe.expense_typeid=e.expense_typeid
+cross apply _periods p
+group by oe.expense_type, p.fin_period
+)
+select 
+	isnull(u.divisionid, o.divisionid) divisionid, 
+	isnull (u.fin_period, o.fin_period) fin_period, 
+	isnull (u.sales_month, lower(format(o.fin_period, 'MMM')) ) sales_month, 
+	isnull (u.sales_year, DATEPART(yyyy, o.fin_period)) sales_year,
+	u.rent_objectid, 
+	isnull(u.rent_type, 'backoffice') charge_type,
+	isnull (u.month_num, DATEPART(MM, o.fin_period)) month_num, 
+	isnull(u.amount, -o.amount) amount, 
+	isnull (u.account, o.account ) account
+from unpvt u
+full join _other o on o.fin_period = u.fin_period and o.divisionid= u.divisionid
 
 go
 
