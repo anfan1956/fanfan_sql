ada4da201f4e2d56729f4bb971f4d0d022f1b2dd
anfan1956 <af.fanfan.2012@gmail.com>
2022-09-06 02:59:02 +0300

    inv.return_all_receipts - all points taken care of
    message returning the receipt types and values

--- sales/inv.return_all_receipts_p.sql
+++ sales/inv.return_all_receipts_p.sql
@@ -18,15 +18,18 @@ set nocount on;
 declare @message varchar (max)= 'Just debugging'
 begin try
 	begin transaction
+
 		
 		declare @barcodes_count int = (select count(*) from @info);
 		declare @saleid int, @returnid int, @customerid int;	
 
+-- если эти баркоды были проданы неоднократно, находим последнюю операцию (top 1) где участвовали
+-- все баркоды и операция - не возврат	
 		with _s as (
 			select i.*, ROW_NUMBER() over(partition by transactionid order by f.barcodeid) num
 			from inv.inventory i 
 				join @info f on f.barcodeid= i.barcodeID
-			where i.logstateID = inv.logstate_id('SOLD')
+			where i.logstateID = inv.logstate_id('SOLD')				
 		)
 		select top 1 @saleid = transactionID 
 		from _s s where s.num=@barcodes_count
@@ -37,24 +40,31 @@ begin try
 				select @note = 'выбранные баркоды относятся к разным по номеру операциям';
 				throw 50001, @note, 1
 			end 
+		else if (select t.transactiontypeID from inv.transactions t where t.transactionID = @saleid) = inv.transactiontype_id('return')
+			begin
+				select @note = 'последняя операция - возврат. Повторный возврат невозможен';
+				throw 50001, @note, 1
+			end 
 
 		select @customerid = customerid from inv.sales where saleID= @saleid;
 			
+-- вычисляем полную сумму к возврату
 		declare @amount money = (select sum(sg.amount)
 		from inv.sales_goods sg
 			join @info b on b.barcodeid=sg.barcodeID
 			 where sg.saleID= @saleid )
 
-
+-- создаем транзакцию "возврат"
 		insert inv.transactions (transactiondate, transactiontypeID, userID)
 		select @date, inv.transactiontype_id ('RETURN'), @userid;
 		select @returnid =SCOPE_IDENTITY();
 
-
+-- создаем продажу
 		insert inv.sales(saleID, customerID, divisionID, salepersonID)
 		select @returnid, @customerid, @divisionid, @userid; 
 
-
+-- создаем чеки возврата в соответствии с тем, как была произведена оплата
+-- в случае смешанной оплаты сначала возвращаем на кредитку, потом наличку
 		with s as (
 			select r.receipttypeID, 
 				s.amount, 
@@ -80,35 +90,43 @@ begin try
 		from t;
 
 
+-- записываем  в  inv.sales_goods с обратным знаком
 		insert inv.sales_goods(saleID, barcodeID, amount, price, client_discount, barcode_discount)
 		select @returnid, g.barcodeID, -g.amount, g.price, g.client_discount, g.barcode_discount 
 		from inv.sales_goods g
 			join @info i on i.barcodeid = g.barcodeID
 		where g.saleID= @saleid;
 
+-- делаем запись в inv.inventory		
 		with _seed (logstateid, divisionid, opersign) as (
 			select inv.logstate_id('SOLD'), null, -1 
 			union 
 			select inv.logstate_id('IN-WAREHOUSE'), @divisionid, 1
 			)
-		--insert inv.inventory (clientID, logstateID, divisionID, transactionID, opersign, barcodeID)
+		insert inv.inventory (clientID, logstateID, divisionID, transactionID, opersign, barcodeID)
 		select 
 			i.clientID, 
 			s.logstateID,
 			isnull (s.divisionID, i.divisionid),
-			i.transactionID,
+			@returnid,
 			s.opersign, 
 			i.barcodeID
 		from inv.inventory i 
 			join @info f on f.barcodeid= i.barcodeID
-			cross apply _seed s
-		where i.transactionID = @saleid and i.logstateID = inv.logstate_id('SOLD');
+				cross apply _seed s
+		where 
+			i.transactionID = @saleid 
+			and i.logstateID = inv.logstate_id('SOLD');
+	
+-- а вот здесь не нужно обновлять всех клиентов, а только того, кто делает возврат
+-- похоже, вообще никого обновлять не нужно
+		--exec cust.totals_update;		
+	select @note = '№ возврата - ' + format(@returnid, '0') + '; ' + STRING_AGG(convert(varchar(max), rt.r_type_rus +'- ' + format(sr.amount, '#,##0;' ) ),  ';  ' ) 
+	from inv.sales_receipts sr 
+		join fin.receipttypes rt on rt.receipttypeID= sr.receipttypeID
+	where sr.saleID= @returnid
 
-		exec cust.totals_update;		
-
-
-	set @note = ''
-	;throw 50001, @message, 1
+	--;throw 50001, @note, 1
 	commit transaction
 end try
 begin catch
@@ -118,16 +136,14 @@ end catch
 go
 
 
-set nocount on; 
-declare @note varchar (max), @saleid int, @userid int= 1;
-declare @divisionid int = 27;
-declare @date datetime = getdate();
-declare @info inv.barcode_type; insert @info values (643080), (651042), (651942)--, (659992)
+--set nocount on; declare @note varchar (max), @userid int = 1; declare @divisionid int = 18; 
+--declare @date datetime = getdate();declare @info inv.barcode_type; insert @info values (653772), (655350); 
+--exec INV.return_all_receipts_p 
+--	@info = @info, 
+--	@userid = @userid, 
+--	@date = @date, 
+--	@divisionid = @divisionid, 
+--	@note = @note output; 
+--select @note
 
-exec inv.return_all_receipts_p
-	@info = @info,
-	@userid = @userid,
-	@date = @date,
-	@divisionid = @divisionid,
-	@note = @note output; 
-select @note
+
