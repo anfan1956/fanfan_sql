3613117a7cfcefa80aa25f861538a0a42fc58050
anfan1956 <af.fanfan.2012@gmail.com>
2022-09-13 01:02:27 +0300

    inv.on_account_return now is not return only but also sale

--- inventory/inv_movements/inv.on_account_return_p.sql
+++ inventory/inv_movements/inv.on_account_return_p.sql
@@ -4,35 +4,87 @@ if object_id('inv.on_account_return_p') is not null drop proc inv.on_account_ret
 go
 
 create proc inv.on_account_return_p 
-	@barcodes barcodes_list readonly, 
+	@barcodes inv.barcode_price_discount_type readonly, 
 	@date date, 
-	@return_to varchar(20), 
+	@transaction_division varchar(20), 
 	@user varchar (50), 
 	@note varchar(max) output, 
+	@cust_discount money, 
+	@receipt_type_id int, 
 	@sales bit = 'False'
 as 
 begin try
 	begin transaction;
 
--- check if return goes to the place barcodes were taken from if @return_to is '' then to the same
--- else to @return_to
+-- check if return goes to the place barcodes were taken from if @transaction_division is '' then to the same
+-- else to @transaction_division
 -- create transaction, CUST_CONSMT_RETURN type
 -- move inventory barcodes to logstate and division
+-- make record in on inv.on_account
+
 		declare @message varchar(max) = 'debuging';
 		declare @divisionid int = 
-			case @return_to 
+			case @transaction_division 
 				when '' then null else 
-			(select divisionID from org.divisions where divisionfullname=@return_to )
+			(select divisionID from org.divisions where divisionfullname=@transaction_division )
 			end;		
 --		select @divisionid;
-		declare @returnid int;
-		if @sales = 'True' throw 50001, @message, 1
+		declare @transactionID int;
+		declare @transactiontype_id int = inv.transactiontype_id('CUST_CONSMT_RETURN');
+		if @sales = 'True' 
+			begin;
+				select @transactiontype_id = inv.transactiontype_id('SALE MIXED');
 
+			end 
+
+--		create transaction
 		insert inv.transactions (transactiondate, transactiontypeID, userID)
-		values (@date, inv.transactiontype_id('CUST_CONSMT_RETURN'), org.person_id(@user));
-		select @returnid = SCOPE_IDENTITY();
+		values (@date, @transactiontype_id, org.person_id(@user));
+		select @transactionID = SCOPE_IDENTITY();
+
+--		insert inv.sales, inv.sales_receipts, inv.sales_goods
+		if @sales = 'True'
+--			для нахождения покупателя по функции объявляем переменную
+			begin;
+				declare @just_barcodes barcodes_list;
+				insert @just_barcodes(barcodeID) select barcodeid from @barcodes;
+				declare @customerid int;
+				select @customerid= customerid from cust.customer_name_barcodes_f (@just_barcodes);
+
+-- insert inv.sales
+				with _s (saleid, divisionid, customerid, salepersonid) as (
+					select @transactionID, d.divisionID, @customerid, org.person_id(@user)
+					from 
+						org.divisions d
+					where d.divisionfullname = @transaction_division
+				)
+				insert inv.sales (saleid, divisionid, customerid, salepersonid)
+				select saleid, divisionid, customerid, salepersonid from _s;
+
+-- insert inv.sales_goods
+				with _s(saleID, barcodeID, amount, price, client_discount, barcode_discount ) as (
+					select 
+						@transactionID, b.barcodeid, b.price *(1-b.discount) *(1 -@cust_discount), 
+						b.price, @cust_discount, b.discount 
+					from @barcodes b
+				)
+				insert inv.sales_goods 
+				select 
+					saleID, barcodeID, amount, price, client_discount, barcode_discount
+				from _s;
+				
+				with _s (saleid, receipttypeid, amount) as (
+					select @transactionID, @receipt_type_id, sum(b.price *(1-b.discount) *(1 -@cust_discount))
+					from @barcodes b
+					
+				)
+				insert inv.sales_receipts (saleID, receipttypeID, amount)
+				select saleID, receipttypeID, amount from _s;
+				declare @amount money = (
+				select sum(amount) from inv.sales_receipts where saleID =  @transactionID);
+			end;
 			
-		
+--		create record in inventory		
 		with _tr as (select top 1 i.transactionID  
 		from inv.inventory i 
 			join @barcodes b on b.barcodeID= i.barcodeID
@@ -46,7 +98,7 @@ begin try
 				when 1 then i.divisionID 
 			end divisionid, 
 --			i.divisionID,
-			@returnid transactionid, 
+			@transactionID transactionid, 
 			-i.opersign opersign,
 			i.barcodeID 
 		from inv.inventory i
@@ -56,11 +108,17 @@ begin try
 		insert inv.inventory (clientID, logstateID, divisionID, transactionID, opersign, barcodeID)
 		select clientID, logstateID, divisionID, transactionID, opersign, barcodeID
 		from s;
-		select @note = 'товар в количестве ' + cast((select count(*) from @barcodes) as varchar (max)) +
-			' шт. принят в магазине' + case when @divisionid is null then ', где он был выдан '
-				else + ' ' + @return_to end
+--		select * from inv.inventory i where i.transactionID=@transactionID
 
-		;throw 50001, @note, 1
+		select @note = 
+			'товар в количестве ' + cast((select count(*) from @barcodes) as varchar (max)) +
+			' шт. ' + iif(@sales = 'True', ' на сумму ' + format(@amount, '#,##0;') + ' руб.', '') + 
+			iif (@sales = 'True', 'продан', 'принят') + ' в магазине' + 
+			case 
+				when @divisionid is null then ', где он был выдан '
+				else + ' ' + @transaction_division end
+
+--		;throw 50001, @note, 1
 	commit transaction
 end try
 begin catch
@@ -70,20 +128,23 @@ end catch
 go
 
 set nocount on; 
-declare @date date = '20220909', 
-@return_to varchar(20) =  '07 ФАНФАН', 
-@user varchar(50) =  'ФЕДОРОВ А. Н.', 
-@note varchar(max); 
-declare @barcodes barcodes_list; 
-insert @barcodes values (636841), (658811), (659785), (662590); 
-exec inv.on_account_return_p 
-	@barcodes = @barcodes, 
-	@date = @date, 
-	@return_to = @return_to , 
-	@user = @user , 
---	@sales= 'True',
-	@note = @note output; 
-select @note; 
-
-
-
+declare 
+	@date date = '20220913', 
+	@transaction_division varchar(20) =  '05 УИКЕНД', 
+	@user varchar(50) =  'БАЛУШКИНА А. А.', 
+	@sales bit = 'True', 
+	@cust_discount money = 0.2,  
+	@receipt_type_id int = 1, 
+	@note varchar(max); 
+declare @barcodes inv.barcode_price_discount_type; 
+insert @barcodes values (658804, 24288, 0.3), (659760, 10560, 0.2); 
+--exec inv.on_account_return_p 
+--	@barcodes = @barcodes, 
+--	@date = @date, 
+--	@transaction_division = @transaction_division , 
+--	@user = @user , 
+--	@sales = @sales, 
+--	@cust_discount = @cust_discount,  
+--	@receipt_type_id = @receipt_type_id, 
+--	@note = @note output; 
+--select @note; 
