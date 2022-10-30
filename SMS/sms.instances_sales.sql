95457de0f92ced59e02416703a5236a7096a9bba
anfan1956 <af.fanfan.2012@gmail.com>
2022-10-30 19:05:58 +0300

    sms.instances_sales to track the sales on promocodes

---
+++ SMS/sms.instances_sales.sql
@@ -0,0 +1,11 @@
+﻿use fanfan
+go
+
+if OBJECT_ID ('sms.instances_sales') is not null drop table sms.instances_sales
+go
+CREATE TABLE sms.instances_sales (
+-- will have to correct the sales procedure
+	saleid int not null constraint fk_sms_instance_sales_sales foreign key references inv.sales (saleid),
+	smsid int not null constraint fk_sms_instance_sales_sms foreign key references sms.instances (smsid), 
+	constraint pk_instances_sales primary key (saleid, smsid)
+)
