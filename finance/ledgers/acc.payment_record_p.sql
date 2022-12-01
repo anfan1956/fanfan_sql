3c59f7f34631214dada090ba813e1f48d74bd541
Alexander FEDOROV <af.fanfan.2012@gmail.com>
2022-12-01 19:46:58 +0300

    added recording begining entries to the payments

--- finance/ledgers/acc.payment_record_p.sql
+++ finance/ledgers/acc.payment_record_p.sql
@@ -18,56 +18,68 @@ begin try
 			@register_debet int, @register_credit int, @transactionid int, 
 			@personid int, @contractorid int;
 
-
-		if (select left(@payee, 14)) = 'номер регистра'
-			select @register_debet = cast(replace (@payee, (left (@payee, 17)), '') as int);
-		select @register_credit = registerid from acc.registers where account = @account;
+		if @payee = 'ОТСУТСТВУЕТ'
+			begin 
+				declare @registerid int = (select registerid from acc.registers where account = @account);
+				
+				with s (entrydate, registerid, amount) as (select @date, @registerid, @amount)
+				merge acc.beg_entries as t using s
+				on t.registerid = s.registerid and t.entrydate=s.entrydate
+				when matched and t.amount<>s.amount 
+					then update set t.amount = s.amount
+				when not matched 
+					then insert (entrydate, registerid, amount)
+					values (entrydate, registerid, amount);
+				if @@ROWCOUNT >0 select @note = 'начальный остаток регистра обновлен' else select @note = 'остаток регистра уже записан'
+			end
+		else
+			begin
+				if (select left(@payee, 14)) = 'номер регистра'
+					select @register_debet = cast(replace (@payee, (left (@payee, 17)), '') as int);
+				select @register_credit = registerid from acc.registers where account = @account;
 		
-		if @article = 'ВЫПЛАТЫ ПЕРСОНАЛУ' 
-			select @personid = personID from org.persons where lfmname = @payee;
-		else 
-			select @contractorid = org.contractor_id(@payee);
+				if @article = 'ВЫПЛАТЫ ПЕРСОНАЛУ' 
+					select @personid = personID from org.persons where lfmname = @payee;
+				else 
+					select @contractorid = org.contractor_id(@payee);
 
---		select @personid, @contractorid;
-
-
-		with s (currencyid, clientid) as (
-			select currencyid, clientid from acc.registers where account = @account
-		)		
-		insert acc.transactions(transdate, bookkeeperid, currencyid, articleid, clientid, amount, comment )
-		select 
-			@date, 
-			org.person_id(@bookkеeper), 
-			s.currencyid, 
-			a.articleid, 
-			s.clientid, 
-			@amount, 
-			@comment
-		from acc.articles a
-			cross apply s
-		where a.article= @article;
+		--		table 1: acc.transactions
+				with s (currencyid, clientid) as (
+					select currencyid, clientid from acc.registers where account = @account
+				)		
+				insert acc.transactions(transdate, bookkeeperid, currencyid, articleid, clientid, amount, comment )
+				select 
+					@date, 
+					org.person_id(@bookkеeper), 
+					s.currencyid, 
+					a.articleid, 
+					s.clientid, 
+					@amount, 
+					@comment
+				from acc.articles a
+					cross apply s
+				where a.article= @article;
 		
-		select @transactionid = SCOPE_IDENTITY();
+				select @transactionid = SCOPE_IDENTITY();
 
-		with _seed (is_credit, accountid, personid, contractorid, registerid) as(
-			select 'True', acc.account_id('Деньги'), null, null, @register_credit
-				union
-			select 'False', accountid, @personid, @contractorid, @register_debet
-			from acc.articles a
-			where a.article= @article
-			)
-		insert acc.entries(transactionid, is_credit, accountid, registerid, personid, contractorid)
-		select @transactionid, s.is_credit, s.accountid,  s.registerid, s.personid, s.contractorid			
-		from _seed s;
---		select * from acc.entries;
+		--		table 2: acc.entries
+				with _seed (is_credit, accountid, personid, contractorid, registerid) as(
+					select 'True', acc.account_id('Деньги'), null, null, @register_credit
+						union
+					select 'False', accountid, @personid, @contractorid, @register_debet
+					from acc.articles a
+					where a.article= @article
+					)
+				insert acc.entries(transactionid, is_credit, accountid, registerid, personid, contractorid)
+				select @transactionid, s.is_credit, s.accountid,  s.registerid, s.personid, s.contractorid			
+				from _seed s;
+
+
 		
-
---		select * from acc.transactions;
---		select @register_credit credit, @register_debet debet, @transactionid transactionid;
-		
-		select @note= 'получатель: ' + @payee + ', сумма: '  + FORMAT(@amount, '#,##0.00') + ' ' + (select c.currencycode  from acc.registers r join cmn.currencies c on c.currencyID= r.currencyid
-			where r.account= @account)
-
+				select @note= 'получатель: ' + @payee + ', сумма: '  + FORMAT(@amount, '#,##0.00') + ' ' 
+					+ (select c.currencycode  from acc.registers r join cmn.currencies c on 
+					c.currencyID= r.currencyid where r.account= @account)
+			end 
 		--throw 50001, @note, 1
 	commit transaction
 end try
@@ -82,6 +94,9 @@ go
 --declare @note varchar(max); exec acc.payment_record_p @note output, '20221128', '40817810900014646072', 'ФЕДОРОВ А. Н.', 'ВЫПЛАТЫ ПЕРСОНАЛУ', 'БАЛУШКИНА А. А.', 'перевод по карте', '10.258'; select @note;
 --go
 --declare @note varchar(max); exec acc.payment_record_p @note output, '20221128', '40817810900014646072', 'ФЕДОРОВ А. Н.', 'ВЫПЛАТЫ ПЕРСОНАЛУ', 'ГОРЛОВА А. Р.', 'перевод по карте', '5600'; select @note;
+--declare @note varchar(max); exec acc.payment_record_p @note output, '20221201', '40817810900014646072', 'ФЕДОРОВ А. Н.', 'НАЧАЛЬНЫЕ ОСТАТКИ', 'ОТСУТСТВУЕТ', 'коррекция', '4561.23'; select @note;
+declare @note varchar(max); exec acc.payment_record_p @note output, '20221201', '40817810900014646072', 'ФЕДОРОВ А. Н.', 'НАЧАЛЬНЫЕ ОСТАТКИ', 'ОТСУТСТВУЕТ', 'коррекция', '13289.25'; select @note;
+
 
 if OBJECT_ID('acc.payments_date_f') is not null drop function  acc.payments_date_f
 go 
@@ -118,4 +133,4 @@ declare @date date = '2022-11-29'
 --select * from acc.entries
 select * from acc.payments_date_f(default)
 select * from acc.transactions
-
+select * from acc.beg_entries_v
