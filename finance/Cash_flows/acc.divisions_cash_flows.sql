d375249eeb08b290e44ea4fae3ecdd4dc6bae788
Alexander FEDOROV <af.fanfan.2012@gmail.com>
2022-12-12 09:43:59 +0300

    acc.divisions_cash_flows init

---
+++ finance/Cash_flows/acc.divisions_cash_flows.sql
@@ -0,0 +1,17 @@
+﻿			with _sales (transdate, amount, account) as (
+				select 
+					cast(t.transactiondate as date),
+					sum (sr.amount * iif(tt.transactiontypeID = 13, -1, 1)),
+					'hc' + d.divisionfullname 
+				from inv.sales_receipts sr
+					join inv.transactions t on t.transactionID= sr.saleID
+					join inv.transactiontypes tt on tt.transactiontypeID=t.transactiontypeID
+					join inv.sales s on s.saleID=sr.saleID
+					join org.divisions d on d.divisionID= s.divisionID
+					join fin.receipttypes rt on rt.receipttypeID=sr.receipttypeID
+				where receipttype like '%cash%'
+					and cast(t.transactiondate as date)>='20220101'
+				group by d.divisionfullname, cast(t.transactiondate as date)
+			)
+			select * from _sales;
+select * from acc.registers
