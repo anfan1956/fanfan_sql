f927003737431ff601c657565d51d19eaf5c406f
Alexander FEDOROV <af.fanfan.2012@gmail.com>
2022-12-15 23:59:10 +0300

    added to acc.beg_entries_around date bookkeeperid

--- finance/ledgers/acc.registers_cashflow_f.sql
+++ finance/ledgers/acc.registers_cashflow_f.sql
@@ -1,4 +1,38 @@
-﻿
+﻿if OBJECT_ID('acc.beg_entries_around_date_f ') is not null drop function acc.beg_entries_around_date_f 
+go
+create function acc.beg_entries_around_date_f (@date date) returns table as return
+	with _before as (
+		select distinct registerid from acc.beg_entries e where e.entrydate<=@date
+	)
+	, _after as (
+		select distinct registerid from acc.beg_entries 
+		except select registerid from _before
+	)
+	, _ordered (registerid, entrydate, entryid, amount, num, bookkeperid) as (
+		select 
+			b.registerid,
+			b.entrydate, 
+			b.entryid, 
+			b.amount, 
+			ROW_NUMBER() over(partition by registerid order by entrydate desc), 
+			b.bookkeeperid
+		from acc.beg_entries b
+		where entrydate <=@date
+	union all 
+		select 
+			b.registerid,
+			b.entrydate, 
+			b.entryid,
+			b.amount, 
+			ROW_NUMBER() over(partition by b.registerid order by entrydate), 
+			b.bookkeeperid
+		from acc.beg_entries b
+			join _after be on be.registerid=  b.registerid
+	)
+	select o.registerid, o.entrydate, o.entryid, o.amount, o.bookkeperid, o.num 
+	from _ordered o  where num =1;
+	go
+
 if OBJECT_ID('acc.registers_cashflow_f')  is not null drop function acc.registers_cashflow_f
 go
 create function acc.registers_cashflow_f (@date date) returns table as return
@@ -66,38 +100,6 @@ go
 declare @date date = '20221128';
 select * from acc.registers_cashflow_f(@date)
 order by 3;
-
-if OBJECT_ID('acc.beg_entries_around_date_f ') is not null drop function acc.beg_entries_around_date_f 
 go
-create function acc.beg_entries_around_date_f (@date date) returns table as return
-	with _before as (
-		select distinct registerid from acc.beg_entries e where e.entrydate<=@date
-	)
-	, _after as (
-		select distinct registerid from acc.beg_entries 
-		except select registerid from _before
-	)
-	, _ordered (registerid, entrydate, entryid, amount, num) as (
-		select 
-			b.registerid,
-			b.entrydate, 
-			b.entryid, 
-			b.amount, 
-			ROW_NUMBER() over(partition by registerid order by entrydate desc)  num
-		from acc.beg_entries b
-		where entrydate <=@date
-	union all 
-		select 
-			b.registerid,
-			b.entrydate, 
-			b.entryid,
-			b.amount, 
-			ROW_NUMBER() over(partition by b.registerid order by entrydate)  num
-		from acc.beg_entries b
-			join _after be on be.registerid=  b.registerid
-	)
-	select * from _ordered  where num =1;
-	go
-
 declare @date date = '20221128';
 select * from acc.beg_entries_around_date_f(@date)
