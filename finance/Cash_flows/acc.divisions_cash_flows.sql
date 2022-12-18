e10efa78c581a009290b576367dee919f6d6b9e8
Alexander FEDOROV <af.fanfan.2012@gmail.com>
2022-12-18 21:31:41 +0300

    division cash flow - removed date hardcoding

--- finance/Cash_flows/acc.divisions_cash_flows.sql
+++ finance/Cash_flows/acc.divisions_cash_flows.sql
@@ -3,14 +3,14 @@ if OBJECT_ID('acc.divisions_cash_f') is not null drop function acc.divisions_cas
 go 
 
 create function acc.divisions_cash_f (@date date) returns table as return
-with _sales (transid, transdate, amount, account, transtype, personid) as (
+with _final (transid, transdate, amount, registerid, shop, transtype, personid) as (
 	select 
 		s.saleID,
 		cast(t.transactiondate as date),
 		sum (sr.amount * iif(tt.transactiontypeID = 13, -1, 1)),
-		replace('hc' + d.divisionfullname, ' ', ''), 
+		r.registerid,
+		d.divisionfullname,
 		tt.transactiontype, 
---		rt.receipttype,
 		s.salepersonID
 	from inv.sales_receipts sr
 		join inv.transactions t on t.transactionID= sr.saleID
@@ -18,30 +18,17 @@ with _sales (transid, transdate, amount, account, transtype, personid) as (
 		join inv.sales s on s.saleID=sr.saleID
 		join org.divisions d on d.divisionID= s.divisionID
 		join fin.receipttypes rt on rt.receipttypeID=sr.receipttypeID
-	where rt.receipttype like '%cash%'
-		and cast(t.transactiondate as date)>='20220101'
+		join acc.registers r on r.account= replace('hc' + d.divisionfullname, ' ', '') 
+		join acc.beg_entries_around_date_f(@date) f 
+			on f.registerid= r.registerid and cast(t.transactiondate as date) >=f.entrydate
+	where rt.r_type_rus	 = 'наличные'
 	group by 
 		s.saleID,
 		d.divisionfullname, 
 		cast(t.transactiondate as date), 
-		tt.transactiontype, 
---		rt.receipttype, 
+		tt.transactiontype,
+		r.registerid, 	 
 		s.salepersonID
-)
-, _final (transid, transdate, amount, registerid, shop, transtype, personid) as (
-	select	
-		s.transid,
-		cast (s.transdate as datetime) transdate,
-		s.amount, 
-		r.registerid, 
-		acc.shop_f(r.registerid) shop, 
-		s.transtype,
-		s.personid
-	from _sales s
-		join acc.registers r on r.account= s.account
-		join acc.beg_entries_around_date_f(@date) f 
-			on f.registerid= r.registerid and s.transdate >=f.entrydate
-
 	union all 
 	select
 		f.entryid,
@@ -77,17 +64,6 @@ from _final f
 go
 
 declare @date date = '20221201';
-/*
-select 
-	t.transactionid, t.transdate, t.amount, t.clientid,  a.article,
-	e.contractorid, e.is_credit, e.personid, e.registerid
-from acc.transactions t 
-	join acc.articles a on a.articleid=t.articleid
-	join acc.entries e on e.transactionid = t.transactionid
-where t.transactionid > 1159
-*/
-
 --declare @note varchar(max), @paymentid int = 1160; exec acc.payment_delete_p @note output,	@paymentid; select @note;
-select * from acc.divisions_cash_f('20221201')
+select * from acc.divisions_cash_f(@date)
 
-select * from acc.beg_entries
