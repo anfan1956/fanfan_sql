72eab01c94b869a44bfac3aba868654f4ea09085
anfan1956 <af.fanfan.2012@gmail.com>
2023-01-29 19:25:02 +0300

    check if the attendances correct

---
+++ hr - salaries, vacations/org.attendance_check_v.sql
@@ -0,0 +1,38 @@
+﻿use fanfan
+go
+if OBJECT_ID ('org.attendance_check_v') is not null drop view org.attendance_check_v
+go
+create view org.attendance_check_v as
+
+	with t as (
+		select a.*, count (attendanceID) over (partition by cast(checktime as date)) num
+		from org.attendance a
+		where a.personID = 67
+		--and year(checktime) = 2023
+	)
+	select 
+		--* 
+		distinct cast(t.checktime as date) irreg_date, 
+		t.personID, p.lfmname person, t.num registrations
+	from t
+		join org.persons p on p.personID =t.personID
+	where t.num % 2 <>0
+go
+
+select a.* 
+from org.attendance_check_v v
+	join org.attendance a on a.personID = v.personID
+		and cast(a.checktime as date) = v.irreg_date
+
+select *
+from org.attendance a 
+where a.personID = 67
+	and cast(a.checktime as date ) = '20230104'
+
+select * 
+from hr.salary_jobs_log l
+--where logid = 29
+order by 2 desc
+
+select  DATEADD(dd, -10,  hr.salary_next_date_f())
+select hr.salary_date_f()
