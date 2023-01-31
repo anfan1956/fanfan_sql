c66425195a3c8f7f72128caeca67aff68124f384
anfan1956 <af.fanfan.2012@gmail.com>
2023-01-31 08:45:35 +0300

    added set nocount on;

--- inventory/history/barcodes_history_log_tables.sql
+++ inventory/history/barcodes_history_log_tables.sql
@@ -2,35 +2,36 @@
 go
 
 
-if OBJECT_ID('hst.barcodes_read') is not null drop table hst.barcodes_read
-if OBJECT_ID('hst.procs') is not null drop table hst.procs
-create table hst.procs (
-	procid int not null identity primary key,
-	procname varchar (25) not null  unique
-)
+--if OBJECT_ID('hst.barcodes_read') is not null drop table hst.barcodes_read
+--if OBJECT_ID('hst.procs') is not null drop table hst.procs
+--create table hst.procs (
+--	procid int not null identity primary key,
+--	procname varchar (25) not null  unique
+--)
  
-create table hst.barcodes_read (
-	logid int not null identity primary key,
-	barcodeid int not null foreign key references inv.barcodes (barcodeid),
-	date_checked datetime default (current_timestamp), 
-	procid int not null foreign key references hst.procs (procid), 
-	workstationid int not null foreign key references org.workstations (workstationid)
-)
+--create table hst.barcodes_read (
+--	logid int not null identity primary key,
+--	barcodeid int not null foreign key references inv.barcodes (barcodeid),
+--	date_checked datetime default (current_timestamp), 
+--	procid int not null foreign key references hst.procs (procid), 
+--	workstationid int not null foreign key references org.workstations (workstationid)
+--)
 
-insert hst.procs (procname) values ('sale_prep'), ('return_prep'), ('info'), ('movement'), ('inv_take')
+--insert hst.procs (procname) values ('sale_prep'), ('return_prep'), ('info'), ('movement'), ('inv_take')
 
 select * from hst.procs
 if OBJECT_ID('hst.barcode_log_post') is not null drop proc hst.barcode_log_post
 go 
 create proc hst.barcode_log_post @barcodeid int, @procname varchar(25), @workstation varchar(25)
 as 
+set nocount on;
 begin
 	insert hst.barcodes_read(barcodeid, procid, workstationid)
 	select @barcodeid,   hst.proc_id(@procname), org.workstation_id(@workstation)
 end 
 go
 
-EXEC hst.barcode_log_post 662892, 'sale_prep', 'SERVER'
+--declare @r int; EXEC @r = hst.barcode_log_post 662892, 'sale_prep', 'SERVER'; select @r
 
 if OBJECT_ID('hst.proc_id') is not null drop function hst.proc_id
 go
