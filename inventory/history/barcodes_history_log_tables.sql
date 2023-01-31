c91e85b1a03e82b50728446d1ef653e40d98d1cf
anfan1956 <af.fanfan.2012@gmail.com>
2023-01-31 07:40:26 +0300

    history barcodes checks

---
+++ inventory/history/barcodes_history_log_tables.sql
@@ -0,0 +1,55 @@
+﻿use fanfan
+go
+
+
+if OBJECT_ID('hst.barcodes_read') is not null drop table hst.barcodes_read
+if OBJECT_ID('hst.procs') is not null drop table hst.procs
+create table hst.procs (
+	procid int not null identity primary key,
+	procname varchar (25) not null  unique
+)
+ 
+create table hst.barcodes_read (
+	logid int not null identity primary key,
+	barcodeid int not null foreign key references inv.barcodes (barcodeid),
+	date_checked datetime default (current_timestamp), 
+	procid int not null foreign key references hst.procs (procid), 
+	workstationid int not null foreign key references org.workstations (workstationid)
+)
+
+insert hst.procs (procname) values ('sale_prep'), ('return_prep'), ('info'), ('movement'), ('inv_take')
+
+select * from hst.procs
+if OBJECT_ID('hst.barcode_log_post') is not null drop proc hst.barcode_log_post
+go 
+create proc hst.barcode_log_post @barcodeid int, @procname varchar(25), @workstation varchar(25)
+as 
+begin
+	insert hst.barcodes_read(barcodeid, procid, workstationid)
+	select @barcodeid,   hst.proc_id(@procname), org.workstation_id(@workstation)
+end 
+go
+
+EXEC hst.barcode_log_post 662892, 'sale_prep', 'SERVER'
+
+if OBJECT_ID('hst.proc_id') is not null drop function hst.proc_id
+go
+create function hst.proc_id (@procname varchar (25)) returns int as
+begin
+declare @procid int
+	select @procid = procid from hst.procs p where p.procname= @procname
+return @procid
+end
+go
+select b.*, v.brandID, br.brand
+
+from hst.barcodes_read b
+	join inv.barcode_info_v v on b.barcodeid=v.barcodeID
+	join inv.brands br on br.brandID = v.brandID
+
+
+
+
+
+
+
---
+++ inventory/history/hst.barcodes_checking_f.sql
@@ -0,0 +1,71 @@
+﻿use fanfan
+go
+
+select * from hst.barcodes_read --where date_checked= @date
+;
+
+if OBJECT_ID('hst.barcodes_checking_f') is not null drop function hst.barcodes_checking_f
+go
+create function hst.barcodes_checking_f (@date as date ) returns table as return
+with _persons  as (
+	select 
+		a.personID, a.workstationID, 
+		a.checktime time_in, a.checktype,
+		lead(a.checktime) over (partition by personid order by checktime) time_out
+	from org.attendance a 
+where cast(checktime as date)= cast(@date as date)
+)
+, _checked as (
+select 
+	b.barcodeid,
+	p.personID, 
+	pr.lfmname, 
+	b.date_checked,
+	--p.workstationID, 
+	--time_in, 
+	--time_out, 
+	wd.divisionfullname division, 
+	b.procid, 
+	ROW_NUMBER() over(partition by b.barcodeid order by b.date_checked desc) num
+from _persons p
+	join hst.barcodes_read b on b.date_checked > p.time_in and b.date_checked < isnull(time_out, getdate())
+	join org.persons pr on pr.personID = p.personID
+	join org.workstations_divisions_current_v wd on wd.workstationid=p.workstationID
+		and wd.datestart < @date	
+where checktype =1
+--group by 	
+--	b.barcodeid,
+--	p.personID, 
+--	pr.lfmname, 
+--	--b.date_checked, 
+--	--p.workstationID, 
+--	--time_in, 
+--	--time_out, 
+--	wd.divisionfullname
+)
+select 
+	c.barcodeid, c.personID, 
+	c.lfmname registered, 
+	c.date_checked last_checked, 
+	c.division division_checked, 
+	pr.procname check_type,  
+	br.brand, it.inventorytype category, s.article, sz.size, cl.color, 	
+	d.divisionfullname current_location
+from _checked c
+	join inv.barcodes b on b.barcodeID = c.barcodeid
+	join inv.styles s on s.styleID = b.styleID
+	join inv.sizes sz on sz.sizeID=b.sizeID
+	join inv.colors cl on cl.colorID=b.colorID
+	join inv.brands br on br.brandID=s.brandID
+	join inv.inventorytypes it on it.inventorytypeID = s.inventorytypeID
+	join inv.v_remains r on r.barcodeID=c.barcodeid
+	join inv.logstates l on l.logstateID= r.logstateID
+	join org.divisions d on d.divisionID=r.divisionID
+	join hst.procs pr on pr.procid=c.procid
+where 
+	l.logstate = 'IN-WAREHOUSE'
+	and num = 1
+go
+
+declare @date date = '2023-01-31'
+select * from hst.barcodes_checking_f(@date)
