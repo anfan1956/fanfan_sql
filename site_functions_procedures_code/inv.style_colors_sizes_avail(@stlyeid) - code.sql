48aeb761b337f1905da428fe5985cbe0cdd9b784
anfan1956 <af.fanfan.2012@gmail.com>
2023-03-19 19:44:13 +0300

    добавил поля, в т. ч. barcodeid

--- site_functions_procedures_code/inv.style_colors_sizes_avail(@stlyeid) - code.sql
+++ site_functions_procedures_code/inv.style_colors_sizes_avail(@stlyeid) - code.sql
@@ -14,22 +14,31 @@ with _art (article, brandid) as (
 	from inv.current_rate_v cr 
 )
 
-, _s (styleid, colorid, sizeid, qty, price, cost, divisionid, division) as (
+, _s (styleid, colorid, sizeid, qty, price, cost, divisionid, division, barcodeid, category) as (
 	select 
 		st.styleID, b.colorID, b.sizeID, count(b.barcodeid), 
 		st.cost * isnull(st.cost_adj, 1) * cr.rate * cr.markup,
 		max(st.cost) over(),
-		r.divisionID, d.division
+		r.divisionID, 
+		d.divisionfullname, 
+		b.barcodeID, 
+		it.inventorytyperus
 	from _art a
 		join inv.styles st on st.article=a.article and	st.brandID=a.brandid
+		join inv.inventorytypes it on it.inventorytypeID=st.inventorytypeID
 		join inv.orders o on o.orderID=st.orderID
 		join inv.barcodes b on b.styleID=st.styleID
 		join inv.v_remains r on r.barcodeID=b.barcodeID and r.logstateID =inv.logstate_id ('IN-WAREHOUSE')
 		join org.active_retail_divisions_f(getdate()) d on d.divisionID=r.divisionID
 		join inv.current_rate_v cr on cr.currencyid = o.currencyID and  cr.divisionID= org.division_id('fanfan.store')
-	group by st.styleID, b.colorID, b.sizeID, st.cost, st.cost_adj, cr.rate, cr.markup, r.divisionid, d.division
+	group by 
+		st.styleID, b.colorID, b.sizeID, 
+		st.cost, st.cost_adj, 
+		cr.rate, cr.markup, 
+		r.divisionid, d.divisionfullname, b.barcodeID, 
+		it.inventorytyperus
 )
-	select sz.size, s.sizeid, cl.color, s.qty, s.price, s.styleid, s.division
+	select sz.size, s.sizeid, cl.color, s.qty, s.price, s.styleid, s.division, s.barcodeid, s.category
 	from _s s
 	join inv.sizes sz on sz.sizeID=s.sizeid
 	join inv.colors cl on cl.colorID=s.colorid
@@ -41,3 +50,5 @@ from inv.style_colors_sizes_avail (@styleid) ORDER BY color ASC, sizeid
 
 select * from inv.styles st  where st.styleid =@styleid;
 
+go
+select * from inv.style_colors_sizes_avail(19996)
