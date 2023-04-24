47292b582b5a1a238760bfeed8a3e6ccbacf9cce
anfan1956 <af.fanfan.2012@gmail.com>
2023-04-24 22:56:57 +0300

    cus.connect and sms.phones

---
+++ Club/cust.phone_update.sql
@@ -0,0 +1,26 @@
+﻿
+if OBJECT_ID ('cust.phone_update') is not null drop proc cust.phone_update
+go 
+create proc cust.phone_update @customerid int, @phone char(10) as
+set nocount on;
+declare @r int;
+begin try
+	begin transaction
+
+		update c set c.connect = @phone
+		from cust.connect c
+		where c.personID = @customerid 
+		and c.connecttypeID = 1;
+
+		update s
+		set s.phone = '7' + @phone  
+		from sms.phones s where s.customerid= @customerid
+	commit transaction
+end try
+begin catch
+	rollback transaction
+	select @r= -1
+	return @r
+end catch
+go
+
