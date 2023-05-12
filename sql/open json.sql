5bf4789404506200506a024246cf69e8e499acc6
anfan1956 <af.fanfan.2012@gmail.com>
2023-05-12 03:44:39 +0300

    init commit email and open_json

---
+++ sql/open json.sql
@@ -0,0 +1,21 @@
+﻿select p.prefix + s.suffix from web.payment_links l join web.link_suffix s on s.linkid= l.linkid cross apply web.link_prefix p where l.orderid=77488
+
+select * from web.link_prefix
+select f.* from web.link_suffix f
+
+declare @par varchar(max) ='{"orderid": "77505", "orderStatus": 2}'
+;
+declare @orderid int, @orderStatus int;
+with s (orderid, orderStatus) as (
+SELECT *
+FROM OPENJSON(@par) WITH (
+    orderid int, -- '$.orderid'
+    orderStatus INT
+    ))
+select @orderid =s.orderid,  @orderStatus = s.orderStatus
+from s;
+
+select @orderid, @orderStatus
+select *
+From inv.site_reservations
+where reservationid= @orderid
---
+++ sql/server_email.sql
@@ -0,0 +1,28 @@
+﻿USE MASTER
+GO
+
+SP_CONFIGURE 'show advanced options', 1
+RECONFIGURE WITH OVERRIDE
+GO
+
+/* Enable Database Mail XPs Advanced Options in SQL Server */
+SP_CONFIGURE 'Database Mail XPs', 1
+RECONFIGURE WITH OVERRIDE
+GO
+
+SP_CONFIGURE 'show advanced options', 0
+RECONFIGURE WITH OVERRIDE
+GO
+
+
+use fanfan
+go
+
+EXECUTE msdb.dbo.sysmail_help_profile_sp;  
+declare @profile_name varchar(max)= 'fanfan_mail_admin'
+
+EXEC msdb.dbo.sp_send_dbmail  
+    @profile_name = @profile_name,  
+    @recipients = 'af.fanfan.2012@gmail.com',  
+    @body = 'NEW SALE!!!',  
+    @subject = 'Automated Success Message' ;  
