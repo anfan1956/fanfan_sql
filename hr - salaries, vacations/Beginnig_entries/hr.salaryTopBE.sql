b595285e48d13ce26d630ba4b793bd474c3d08ca
anfan1956 <af.fanfan.2012@gmail.com>
2023-10-29 21:10:57 +0300

    distinct from salary BE

--- hr - salaries, vacations/Beginnig_entries/hr.salaryTopBE.sql
+++ hr - salaries, vacations/Beginnig_entries/hr.salaryTopBE.sql
@@ -1,7 +1,8 @@
 ﻿if OBJECT_ID('hr.salaryTopBE_date_f') is not null drop function hr.salaryTopBE_date_f
 go 
 create function hr.salaryTopBE_date_f (@date date) returns table as return
-	with s (personid, entrydate, document, clientid, amount, num) as (
+with 
+ s (personid, entrydate, document, clientid, amount, num) as (
 	select employeeid, entrydate, 
 		case t.salarytype
 			when 'касса' then 'cash'
@@ -31,7 +32,15 @@ select * from hr.salaryTopBE_date_f(@date) where personid = 1075
 if OBJECT_ID('rep.salaryReport_BEdate_f') is not null drop function rep.salaryReport_BEdate_f
 go
 create function rep.salaryReport_BEdate_f(@date date) returns table as return
-with s as (select 
+with 
+active as (
+	select distinct s.personid
+	from hr.schedule_21 s
+	join org.persons p on p.personID=s.personid
+	where s.date_finish is null or DATEDIFF(mm, s.date_finish, GETDATE())< 2
+	)
+,
+s as (select 
 	e.personid,
 	cast(t.transdate as datetime) transdate,
 	t.articleid,
@@ -43,7 +52,8 @@ with s as (select
 from acc.entries e
 	join acc.entries e2 on e2.transactionid = e.transactionid and e2.is_credit<>e.is_credit
 	join acc.transactions t on t.transactionid=e.transactionid
-	join hr.salaryTopBE_date_f (@date) s on 
+--	join hr.salaryTopBE_date_f (@date) s on 
+	join (select distinct personid, entrydate, articleid, document, comment, amount from hr.salaryTopBE_date_f(@date)) s on 
 		s.personid=e.personid and
 		s.document=t.document and
 		t.transdate>=s.entrydate
@@ -75,24 +85,10 @@ select
 	trim(документ) документ, 
 	сумма, reg_id, банк, счет_банк
 from _united u
-where u.personid not in  (org.person_id('РОМАХИН М. Е.'), org.person_id('НЕВЕРОВ А. Д.'))
+join active s on s.personid =u.personid
 go
 
 
 declare @date date = '20231201';
 select * from rep.salaryReport_BEdate_f(@date) s
-where s.personid =1075
---select * from rep.payroll_report_v
---select * from hr.salary_BegEntries s where employeeid = 1; select * from hr.salaryTopBE_date_f(@date) where personid = 1;
-
---with s (personid, дата, документ, комментарий, num) as (
---select 
---	v.personid, v.дата, v.документ, v.комментарий, 
---	ROW_NUMBER () over (partition by personid, документ order by дата) num
---from rep.payroll_report_v v 
---where v.документ in ('bank', 'cash')
---)
---select * from s where num =1
-
-
-
+where s.personid =1073
