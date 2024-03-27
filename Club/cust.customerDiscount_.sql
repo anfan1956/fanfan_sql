49da2173b72e8d913ed81496a63f34a875354faa
anfan1956 <af.fanfan.2012@gmail.com>
2024-03-27 03:40:13 +0300

    customer discount

--- Club/cust.customerDiscount_.sql
+++ Club/cust.customerDiscount_.sql
@@ -36,7 +36,7 @@ begin
 
 	select @discount = d.discount 
 	from _level l
-		join cust.discount_levels d on d.discountlevelID=l.discountlevelid
+		join cust.discount_levels d on d.discountlevelID=iif(l.discountlevelid=1, 2, l.discountlevelid)
 	return @discount
 end
 go
@@ -44,5 +44,38 @@ go
 declare @personid int = 
 	--14995;
 	--17460 
-	4
+	17261
+	--4
 select cust.customerDiscount_(@personid)
+
+	; with _lastYear as (
+		select 		isnull(sum (g.amount), 0) total
+		from cust.persons p
+			left join inv.sales s on s.customerID=p.personID
+			left join inv.sales_goods g on g.saleID=s.saleID
+			left join inv.transactions t on t.transactionID=s.saleID 
+		where p.personID=@personid 
+			and t.transactiondate>=DATEADD(YYYY, -1, dbo.justdate(getdate()))
+
+		)
+		, _earned as (select l.discountlevelID, l.min_amount, y.total
+						from cust.discount_levels l
+							cross apply _lastYear y   
+						where l.discountlevelID <=20
+		)
+	, _all as (select discountlevelID , ROW_NUMBER() over (order by discountlevelid desc) num
+				from _earned where total>=min_amount
+			)
+	, _level as (
+			select top 1 
+				case pp.customermode
+					when 'M' then pp.discountlevelID
+					when 'A' then (select discountlevelID from _all where num =1)
+					end discountlevelid
+			from cust.persons_programs pp
+				cross apply _all a
+			where pp.personID = @personid
+	)
+select * 
+from _level l
+	join cust.discount_levels d on d.discountlevelID=iif(l.discountlevelid=1, 2, l.discountlevelid)
