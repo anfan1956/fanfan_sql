c27e40505987f1da3756d2ba0afbbfe966e15a9b
anfan1956 <af.fanfan.2012@gmail.com>
2024-10-23 23:28:04 +0300

    Added option to delete employees hours

--- hr - salaries, vacations/attendane/proc org.attendanceFull_p.sql
+++ hr - salaries, vacations/attendane/proc org.attendanceFull_p.sql
@@ -7,45 +7,55 @@ create proc  org.attendanceFull_p
 			@date datetime
 		,	@personID int 
 		,	@workstationID int
+		,	@delete bit = 'False'
 as 
 set nocount on;
 set transaction isolation level read committed;
 declare @msg varchar (max)
-begin try  
-	begin transaction
-		declare 
-				@sTime TIME		= '10:00'
-			,	@eTime TIME		= '22:00'
+	begin try  
+		begin transaction
+			declare 
+					@sTime TIME		= '10:00'
+				,	@eTime TIME		= '22:00'
 
-		delete a
-		from org.attendance a
-		where	a.workstationID				= @workstationID
-			and	a.personID					= @personID
-			and cast(a.checktime as date )	= @date;
+			delete a
+			from org.attendance a
+			where	a.workstationID				= @workstationID
+				and	a.personID					= @personID
+				and cast(a.checktime as date )	= @date;
+		set @delete = isnull(@delete, 'False')
+		if @delete = 'False'
+		begin
+			with _seed (checktype, checktime) as (
+				select 1,  cast(@date as datetime) + cast(@sTime as datetime)
+				union all 
+				select 0,  cast(@date as datetime) + cast(@eTime as datetime)
+			)
+			insert org.attendance (personID, checktime, checktype, workstationID, superviserID)
+			select @personID, s.checktime, s.checktype, @workstationID, 1 superviserID
+			from _seed s;
+		end
+	--	;throw 50001, 'debuging' , 1 
+		select @msg = @@ROWCOUNT
+		select @msg msg
+		commit transaction
+	end try
 
-		with _seed (checktype, checktime) as (
-			select 1,  cast(@date as datetime) + cast(@sTime as datetime)
-			union all 
-			select 0,  cast(@date as datetime) + cast(@eTime as datetime)
-
-		)
-		insert org.attendance (personID, checktime, checktype, workstationID, superviserID)
-		select @personID, s.checktime, s.checktype, @workstationID, 1 superviserID
-		from _seed s;
---	;throw 50001, 'debuging' , 1 
-	select @msg = @@ROWCOUNT
-	select @msg msg
-	commit transaction
-end try
-begin catch
-	select @msg = ERROR_MESSAGE()
-	select @msg
-	rollback transaction
-end catch
+	begin catch
+		select @msg = ERROR_MESSAGE()
+		select @msg
+		rollback transaction
+	end catch
 go 
 
-declare @date datetime = '20241019', @personid int = 1078, @workstationID int = 14
-select a.*, p.lfmname 
+
+declare 
+	@date datetime = '20241022' 
+	, @personid int = 1078 
+	, @workstationID int = 14
+	, @delete bit = 'False'
+
+;select a.*, p.lfmname 
 from org.attendance a
 	join org.persons p on p.personID =a.personID 
 where cast(a.checktime as date) = @date
@@ -55,6 +65,10 @@ exec org.attendanceFull_p
 		@date 		= @date	
 	, 	@personID	= @personID
 	,	@workstationID  = @workstationID
+	,	@delete  = @delete
 ;
 */
-        
+;select a.*, p.lfmname 
+from org.attendance a
+	join org.persons p on p.personID =a.personID 
+where cast(a.checktime as date) = @date        
