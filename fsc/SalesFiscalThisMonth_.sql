ab6be4c00d2f5dab6911a35aa0444b15a9009f6d
anfan1956 <af.fanfan.2012@gmail.com>
2025-01-26 22:42:26 +0300

    добавил redirectTo

--- fsc/SalesFiscalThisMonth_.sql
+++ fsc/SalesFiscalThisMonth_.sql
@@ -3,16 +3,24 @@ go
 create function acc.SalesFiscalThisMonth_(@division varchar(255)) returns money as 
 begin 
 	declare @sales money;
+
+with ref as (
+	select s.saleID 
+	from acc.CardRedirectLog l 
+		join inv.sales s on s.saleID=l.transactionId
+	where s.divisionID = org.division_id(@division)
+)
 	select @sales  = sum(sr.amount) 
-	from inv.sales s 
-		join inv.sales_receipts sr on sr.saleID = s.saleID
-		join inv.transactions t on t.transactionID= s.saleID
-	where 1 = 1
-		and s.divisionID = org.division_id(@division)
-		and MONTH(t.transactiondate) = MONTH(GETDATE())
-		and YEAR(t.transactiondate)= YEAR (GETDATE())
-		and receiptid is not null
-	group by s.divisionID
+from inv.sales s 
+	join inv.sales_receipts sr on sr.saleID = s.saleID
+	join inv.transactions t on t.transactionID= s.saleID
+where 1 = 1
+	and s.divisionID = org.division_id(@division)
+	and MONTH(t.transactiondate) = MONTH(GETDATE())
+	and YEAR(t.transactiondate)= YEAR (GETDATE())
+	and isnull(receiptid, 0) <> 0
+	and s.saleID not in (select saleID from ref)
+		
 	return @sales
 end 
 
