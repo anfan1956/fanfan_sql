a1b3d51908babfb833c13f59d0288fdb8d0b82be
unknown <alexander.n.fedorov@gmail.com>
2025-03-07 15:59:19 +0300

    corrected acc.ConsignmentAPs completely

--- contractor_accounts/acc.ConsignmentAPs.sql
+++ contractor_accounts/acc.ConsignmentAPs.sql
@@ -1,106 +1,81 @@
-﻿if OBJECT_ID ('acc.ConsignmentAPs') is not null drop function acc.ConsignmentAPs
+﻿use fanfan
+go
+if OBJECT_ID ('acc.ConsignmentAPs') is not null drop function acc.ConsignmentAPs
 go 
 
 create function acc.ConsignmentAPs (@startdate date = '20241101', @vendor varchar(max)) returns table as 
 return
 
-	with  _barcodes  (barcodeid ,  vendorid) as (
-		select distinct 
-			barcodeid, o.showroomID
-		from inv.inventory i 
-			join inv.orders o on o.orderID = i.transactionID and o.orderclassID =3
-			join inv.transactions t on t.transactionID= o.orderID
-		where 1=1
-			and t.transactiondate >=@startDate
-		)
-	, _sales (saleid, article, shop, SC, category, cost, barcodeid, vendorid, transtype)  as 
-		(
-		select 
-			s.saleID
-			, st.article
-			, d.divisionfullname
-			, d.comment, it.inventorytyperus
-			, case tt.transactiontype 
-				when 'RETURN'  
-					then -st.cost
-				else 
-					st.cost 
-			end		
-			, sg.barcodeid 
-			, bc.vendorid 
-			, tt.transactiontype
-		from inv.sales s
-			join inv.sales_goods sg on sg.saleID =s.saleID
-			join _barcodes bc on bc.barcodeid= sg.barcodeID
-			join inv.barcodes b on b.barcodeID = bc.barcodeid
-			join inv.styles st on st.styleID=b.styleID
-			join org.divisions d on d.divisionID=s.divisionID
-			join inv.inventorytypes it on it.inventorytypeID=st.inventorytypeID
-			join inv.transactions t on t.transactionID = s.saleID
-			join inv.transactiontypes tt on tt.transactiontypeID=t.transactiontypeID
-		
-	)
-, final (
-	transactiondate
-	, transactionid
-	, tranDate
-	, transType
-	, division
-	, sc
-	, category
-	, article
-	, amount
-	, vendorid
+	select 
+		 sr.transactionid	
+		, sr.tranDate	
+		, sr.transType	
+		, sr.division	
+		, sr.SC	
+		, sr.category	
+		, sr.article	
+		, sr.amount
+		, sr.transactionDate
+	from 
+	(
+	select 
+		 transactionid =		sg.saleID
+		, tranDate =			format( t.transactiondate, 'dd.MM.yyyy')
+		, transType =			tt.transactiontype
+		, division =			divisionfullname
+		, SC =					d.comment
+		, category =			it.inventorytyperus
+		, article  =			st.article
+		, amount =				st.cost *	
+									case tt.transactiontype
+										when 'SALE' then 1
+										else -1
+									end
+		, transactionDate = cast(t.transactiondate as date)
 
-) as (
+	from inv.sales_goods sg
+		join inv.transactions t on t.transactionID=sg.saleID
+		join inv.transactiontypes tt on tt.transactiontypeID=t.transactiontypeID
+		join inv.barcodes b on b.barcodeID=sg.barcodeID
+		join inv.styles st on st.styleID = b.styleID
+		join inv.brands br on br.brandID = st.brandID
+		join inv.sales s on s.saleID=sg.saleID
+		join org.divisions d on d.divisionID = s.divisionID
+		join inv.inventorytypes it on it.inventorytypeID=st.inventorytypeID	
+		outer apply (
+			select top 1
+				bra.brandID
+			from inv.brands bra
+				join inv.styles sty on sty.brandID=bra.brandID
+				join inv.orders ord on ord.orderID=sty.orderID
+			where showroomID=org.contractor_id(@vendor)
+			group by bra.brandid	
+		) as bnd
+		where 1=1
+			and t.transactiondate>=@startdate
+			and st.brandID= bnd.brandid
+	/*
+	*/
+	union all
 	select
-		t.transdate
-		, t.transactionid
-		, FORMAT(t.transdate , 'dd.MM.yyyy') tranDate
-		, s.transtype
-		, shop, SC
-		, category
-		, article
-		, cost amount
-		, vendorid 
-	from acc.transactions t 
-				join _sales s on t.saleid =s.saleid 
-		and t.barcodeid =s.barcodeid
-		and t.articleid in 
-				( acc.article_id('закупочная стоимость'), acc.article_id('за консигн. товар'))
-	union all 
-	select
-		t.transdate
-		, t.transactionid
-		, FORMAT(t.transdate, 'dd.MM.yyyy')  transdate
-		, case e.is_credit 
-			when 'True' then 'CREDIT'
-			else 'PAYMENT' end transtype
-		, 'ИП Иванова Т. К.'  
-		, t.comment	
-		, NULL
-		, NULL
-		, case e.is_credit 
-			when 'True' then amount
-			else - amount end amount
-		, b.vendorid
+		transactionid		= t.transactionid 
+		, tranDate			=  format (t.transdate, 'dd.MM.yyyy')
+		, transType			=  'PAYMENT' 
+		, division			= 'ИП Иванова Т. К.'  
+		, sc				= null
+		, category			= null
+		, article			= null
+		, amount			= -amount 
+		, trasactionDate	=  cast (t.transdate as date)
 	from acc.transactions t
 		join acc.entries e on e.transactionid = t.transactionid
-		join _barcodes b on b.vendorid = e.contractorid	
-	where 1=1
-		and e.accountid =7
-		and t.saleid is null
-	group by 
-		vendorid
-		, e.is_credit
-		, t.transactionid, t.transdate, t.amount, t.comment	
-	)
-	select f.transactiondate, f.transactionid, f.tranDate, f.transtype, division, sc, category, article, amount 
-		, vendor = c.contractor
-	from final f
-	join org.contractors c on c.contractor = @vendor
-
+			and is_credit = 'False'
+			and e.contractorid = org.contractor_id('ИП Карпинская Анастасия')
+	where t.transdate>=@startdate
+	) as sr
 go
 
-select c.transactionid, c.tranDate, c.transType, c.division, c.sc, category, c.article, c.amount from acc.ConsignmentAPs (default, 'ИП Карпинская Анастасия') c order by c.transactiondate desc, transactionid 
+select c.transactionid, c.tranDate, c.transType, c.division, c.sc, category, c.article, c.amount 
+from acc.ConsignmentAPs (default, 'ИП Карпинская Анастасия') c 
 
+order by c.transactiondate desc, transactionid 
