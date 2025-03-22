1546908a6aacc9a09481691f45c5af733e18ea43
unknown <alexander.n.fedorov@gmail.com>
2025-03-22 14:24:31 +0300

    попарвил отчет

--- contractor_accounts/acc.ConsignmentAPs.sql
+++ contractor_accounts/acc.ConsignmentAPs.sql
@@ -10,18 +10,22 @@ return
 		 sr.transactionid	
 		, sr.tranDate	
 		, sr.transType	
+		, barcode
 		, sr.division	
 		, sr.SC	
 		, sr.category	
 		, sr.article	
 		, sr.amount
 		, sr.transactionDate
+		, sr.orderDate
+		, sr.orderType
 	from 
 	(
 	select 
-		 transactionid =		sg.saleID
+		 transactionid		=	sg.saleID
 		, tranDate =			format( t.transactiondate, 'dd.MM.yyyy')
 		, transType =			tt.transactiontype
+		, barcode =				b.barcodeID
 		, division =			divisionfullname
 		, SC =					d.comment
 		, category =			it.inventorytyperus
@@ -32,6 +36,8 @@ return
 										else -1
 									end
 		, transactionDate = cast(t.transactiondate as date)
+		, orderDate = tr.transactiondate 
+		, orderType = oc.orderclass
 
 	from inv.sales_goods sg
 		join inv.transactions t on t.transactionID=sg.saleID
@@ -42,18 +48,23 @@ return
 		join inv.sales s on s.saleID=sg.saleID
 		join org.divisions d on d.divisionID = s.divisionID
 		join inv.inventorytypes it on it.inventorytypeID=st.inventorytypeID	
+		join inv.orders o on o.orderID=st.orderID 
+		join inv.transactions tr on tr.transactionID=o.orderID
+		join inv.orderclasses oc on oc.orderclassid= o.orderclassid
 		outer apply (
 			select top 1
 				bra.brandID
 			from inv.brands bra
 				join inv.styles sty on sty.brandID=bra.brandID
 				join inv.orders ord on ord.orderID=sty.orderID
-			where showroomID=org.contractor_id(@vendor)
+			where 1=1
+				and showroomID=org.contractor_id(@vendor)				
 			group by bra.brandid	
 		) as bnd
 		where 1=1
 			and t.transactiondate>=@startdate
 			and st.brandID= bnd.brandid
+			
 	/*
 	*/
 	union all
@@ -61,21 +72,49 @@ return
 		transactionid		= t.transactionid 
 		, tranDate			=  format (t.transdate, 'dd.MM.yyyy')
 		, transType			=  'PAYMENT' 
+		, null
 		, division			= 'ИП Иванова Т. К.'  
 		, sc				= null
 		, category			= null
 		, article			= null
 		, amount			= -amount 
 		, trasactionDate	=  cast (t.transdate as date)
+		, orderDate = null
+		, orderType = null
 	from acc.transactions t
 		join acc.entries e on e.transactionid = t.transactionid
 			and is_credit = 'False'
 			and e.contractorid = org.contractor_id('ИП Карпинская Анастасия')
+			and e.accountid = acc.account_id('Счета к оплате')
 	where t.transdate>=@startdate
 	) as sr
 go
 
-select c.transactionid, c.tranDate, c.transType, c.division, c.sc, category, c.article, c.amount 
-from acc.ConsignmentAPs (default, 'ИП Карпинская Анастасия') c 
 
+
+
+select c.transactionid, c.tranDate, c.transType, c.division, c.sc, category, c.article, c.amount, c.barcode, c.orderDate, c.orderType 
+from acc.ConsignmentAPs (default, 'ИП Карпинская Анастасия') c order by c.transactiondate desc, transactionid 
+
+
+
+
+
+
+declare @bid int = 629038;
+
+select 
+	s.*	
+from inv.styles s 
+	join inv.barcodes b on b.styleid = s.styleid
+where b.barcodeid = @bid
+
+
+select 
+	c.transactionid, c.tranDate, c.transType
+	, barcode
+	, c.division, c.sc, category, c.article, c.amount, orderDate, orderType
+-- этот отчет подходит пока только для Canoe
+from acc.ConsignmentAPs (default, 'ИП Карпинская Анастасия') c 
+--where orderType ='regular'
 order by c.transactiondate desc, transactionid 
