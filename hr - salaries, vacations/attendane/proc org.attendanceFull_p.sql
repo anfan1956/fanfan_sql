e677a1174d57b5bb1e255415bdd2aa383ff1443b
unknown <alexander.n.fedorov@gmail.com>
2025-09-30 21:19:08 +0300

    salary

--- hr - salaries, vacations/attendane/proc org.attendanceFull_p.sql
+++ hr - salaries, vacations/attendane/proc org.attendanceFull_p.sql
@@ -58,12 +58,12 @@ go
 
 
 declare 
-	@date datetime = '20250922'
-	, @personid int = 5
+	@date datetime = '20250928'
+	, @personid int = 10
 
 	, @workstationID int = 23
 	, @delete bit = 'False'
-	, @full bit		= 'True'
+	, @full bit		= 'False'
 
 ;select a.*, p.lfmname 
 from org.attendance a
