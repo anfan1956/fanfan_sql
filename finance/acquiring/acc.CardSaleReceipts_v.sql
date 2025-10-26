d5dbc55c30d8fb833a84db2378ddfeecb9a76fd0
unknown <alexander.n.fedorov@gmail.com>
2025-10-26 10:13:49 +0300

    added date to acc.CardSaleReceipts_v

--- finance/acquiring/acc.CardSaleReceipts_v.sql
+++ finance/acquiring/acc.CardSaleReceipts_v.sql
@@ -8,28 +8,31 @@ if OBJECT_ID ('acc.CardSaleReceipts_v') is not null drop view acc.CardSaleReceip
 go 
 create view acc.CardSaleReceipts_v as
 select 
-	LOWER( tt.transactiontype) ttype
-	, l.transactionId transId
-	, p.lfmname person
-	, rt.r_type_rus recType
-	, sg.barcodeID barcode
-	, it.inventorytyperus invType
+	LOWER( tt.transactiontype)			as ttype
+	, l.transactionId					as transId
+	, t.transactiondate					as saleDate
+	, p.lfmname							as person
+	, rt.r_type_rus						as recType
+	, sg.barcodeID						as barcode
+	, it.inventorytyperus				as invType
 	, sg.amount 
 from acc.CardRedirectLog l
-	join inv.sales s on s.saleID = l.transactionId
-	join org.persons p on p.personID =s.salepersonID
-	join inv.sales_goods sg on sg.saleID=s.saleID
-	join inv.barcodes b on b.barcodeID=sg.barcodeID
-	join inv.styles st on st.styleID = b.styleID
-	join inv.inventorytypes it on it.inventorytypeID=st.inventorytypeID	
-	join inv.transactions t on t.transactionID=s.saleID
-	join inv.transactiontypes tt on tt.transactiontypeID=t.transactiontypeID
-	join inv.sales_receipts sr on sr.saleID=s.saleID
-	join fin.receipttypes rt on rt.receipttypeID=sr.receipttypeID
+	join inv.sales s					on s.saleID = l.transactionId
+	join org.persons p					on p.personID =s.salepersonID
+	join inv.sales_goods sg				on sg.saleID=s.saleID
+	join inv.barcodes b					on b.barcodeID=sg.barcodeID
+	join inv.styles st					on st.styleID = b.styleID
+	join inv.inventorytypes it			on it.inventorytypeID=st.inventorytypeID	
+	join inv.transactions t				on t.transactionID=s.saleID
+	join inv.transactiontypes tt		on tt.transactiontypeID=t.transactiontypeID
+	join inv.sales_receipts sr			on sr.saleID=s.saleID
+	join fin.receipttypes rt			on rt.receipttypeID=sr.receipttypeID
 where closedTime is null
 go
 
-select transId, ttype, recType, person, barcode, invType, amount from acc.CardSaleReceipts_v 
+select transId, saleDate, ttype, recType, person, barcode, invType, amount, sum (amount) over()
+from acc.CardSaleReceipts_v 
+	order by 1 desc
 select * from acc.CardRedirectLog
-select * from inv.sales s order by 1 desc
+
 
